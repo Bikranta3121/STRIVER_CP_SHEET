10266f8feccbd9d966cccf7c197d9c2f0f970d9f
BIKRANTA KUMAR BEHERA <106620478+Bikranta3121@users.noreply.github.com>
2022-11-06 12:04:57 +0530

    Create 28.cpp

---
+++ Implementation and constructive/28.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    
+    string s;
+    cin>>s;
+    unordered_set<char> vowel={'A','a','E','e','I','i','O','o','U','u','Y','y'};
+    string res;
+    for(char ch:s)
+    {
+        if(vowel.find(ch)!=vowel.end())
+         continue;
+        else
+        {
+            res+='.';
+            res+=tolower(ch);
+        }
+    }
+    cout<<res;
+    // int t;
+    // cin>>t;
+    // while(t--)
+    // {
+    //     int n,m;
+    //     cin>>n>>m;
+    //     char a[n][m];
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         for(int j=0;j<m;j++)
+    //         {
+    //             cin>>a[i][j];
+    //         }
+    //     }
+    //     int count=0;
+    //     for(int j=0;j<m-1;j++)
+    //     {
+    //         if(a[n-1][j]!='R')
+    //          count++;
+    //     }
+    //     for(int i=0;i<n-1;i++)
+    //     {
+    //         if(a[i][m-1]!='D')
+    //          count++;
+    //     }
+    //     cout<<count<<endl;
+    //   }
+    
+    return 0;
+}
