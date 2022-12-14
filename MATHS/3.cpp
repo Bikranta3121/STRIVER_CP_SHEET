1bac0c339ea84b98595a4c53c166ba5cdaebcbed
BIKRANTA KUMAR BEHERA <106620478+Bikranta3121@users.noreply.github.com>
2022-12-14 11:47:22 +0530

    Create 3.cpp

---
+++ MATHS/3.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+  int n;
+  cin>>n;
+  if(n%2==0)
+   cout<<"Mahmoud";
+  else
+   cout<<"Ehab";
+  cout<<endl; 
+  
+  
+  
+  
+  
+  
+  
+  
+  /*while(t--)
+  {
+   long long int x,y;
+   cin>>x>>y;
+   if(x*2<=y)
+    cout<<x<<" "<<2*x;
+   else 
+    cout<<-1<<" "<<-1;
+   cout<<endl;
+  }*/
+  return 0;
+}
